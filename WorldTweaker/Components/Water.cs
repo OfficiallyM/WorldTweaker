f65a7ed0a91297a963fd278a576f15af166f31f4
unknown <miner717@hotmail.com>
2026-03-12 20:58:05 +0000

    feat: Add swimming

--- WorldTweaker/Components/Water.cs
+++ WorldTweaker/Components/Water.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using WorldTweaker.Utilities;
 
 namespace WorldTweaker.Components
@@ -8,16 +9,50 @@ namespace WorldTweaker.Components
 	{
 		public Material material;
 
-		private Dictionary<Rigidbody, (float drag, float angularDrag)> _originalDrag = new Dictionary<Rigidbody, (float, float)>();
+		public static bool IsPlayerSwimming
+		{
+			get
+			{
+				return _playerDepth >= 0.4f;
+			}
+		}
+
+		private static Slider _drown;
+		private static float _playerDepth;
+
+		private Dictionary<Rigidbody, (float drag, float angularDrag, float jumpForce)> _originalValues = new Dictionary<Rigidbody, (float, float, float)>();
+
+		public void OnGUI()
+		{
+			var rb = mainscript.M.player?.RB;
+			GUI.Button(new Rect(0, 0, 300, 30), $"Depth: {_playerDepth}");
+			GUI.Button(new Rect(0, 35, 300, 30), $"Swimming: {IsPlayerSwimming}");
+			GUI.Button(new Rect(0, 70, 300, 30), $"Velocity Y: {rb?.velocity.y:F3}");
+			GUI.Button(new Rect(0, 105, 300, 30), $"Drag : {rb?.drag:F3}");
+		}
 
 		public void LateUpdate()
 		{
 			material.mainTextureOffset += new Vector2(Mathf.PerlinNoise(Time.timeSinceLevelLoad * 0.1f, 0.0f) - 0.5f, Mathf.PerlinNoise(250f, Time.timeSinceLevelLoad * 0.1f) - 0.5f) * Time.deltaTime * 0.005f;
 
-			// Keep drag dict clean.
-			foreach (var key in new List<Rigidbody>(_originalDrag.Keys))
+			// Keep original values dictionary clean.
+			foreach (var key in new List<Rigidbody>(_originalValues.Keys))
 				if (key == null)
-					_originalDrag.Remove(key);
+					_originalValues.Remove(key);
+
+			// Disable gravity when swimming.
+			fpscontroller player = mainscript.M.player;
+			if (IsPlayerSwimming && player.RB != null)
+				player.RB.useGravity = false;
+
+			// Reduce drown meter when out of water.
+			if (_drown != null && !IsPlayerSwimming)
+			{
+				_drown.value -= 0.004f * Time.deltaTime;
+
+				if (_drown.value <= 0f)
+					_drown.gameObject.SetActive(false);
+			}
 		}
 
 		public void SetScale(float scale)
@@ -31,13 +66,16 @@ namespace WorldTweaker.Components
 			if (collider.attachedRigidbody == null)
 				return;
 			var rb = collider.attachedRigidbody;
-			if (_originalDrag.ContainsKey(rb))
+			if (_originalValues.ContainsKey(rb))
 				return;
+			var player = mainscript.M.player;
+			_originalValues[rb] = (rb.drag, rb.angularDrag, player.RB == rb ? player.FjumpForce : 0);
 
-			if (rb == mainscript.M.player.RB)
-				Logging.LogDebug($"Stored drag: {rb.drag}, angular drag: {rb.angularDrag}");
-
-			_originalDrag[rb] = (rb.drag, rb.angularDrag);
+			if (player.RB == rb)
+			{
+				// Prevent the player from jumping when in water.
+				player.FjumpForce = 0f;
+			}
 		}
 
 		public void OnTriggerExit(Collider collider)
@@ -45,13 +83,19 @@ namespace WorldTweaker.Components
 			if (collider.attachedRigidbody == null)
 				return;
 			var rb = collider.attachedRigidbody;
-			if (_originalDrag.TryGetValue(rb, out var original))
+			var player = mainscript.M.player;
+			if (_originalValues.TryGetValue(rb, out var original))
 			{
-				if (rb == mainscript.M.player.RB)
-					Logging.LogDebug($"Restored drag: {original.drag}, angular drag: {original.angularDrag}");
 				rb.drag = original.drag;
 				rb.angularDrag = original.angularDrag;
-				_originalDrag.Remove(rb);
+				if (player.RB == rb)
+					player.FjumpForce = original.jumpForce;
+				_originalValues.Remove(rb);
+			}
+			if (player.RB == rb)
+			{
+				_playerDepth = 0;
+				player.RB.useGravity = true;
 			}
 		}
 
@@ -61,29 +105,95 @@ namespace WorldTweaker.Components
 				return;
 
 			var rb = collider.attachedRigidbody;
-			float depth = Mathf.Max(0f, transform.position.y - collider.transform.position.y);
+			float depth = Mathf.Max(0f, transform.position.y - rb.transform.position.y);
 
 			UpdateDrag(rb, depth);
+			UpdatePlayer(rb, depth);
 		}
 
 		private void UpdateDrag(Rigidbody rb, float depth)
 		{
-			// Return early if the original drag values aren't cached
+			// Return early if the original values aren't cached
 			// to prevent the water values from caching.
-			if (!_originalDrag.ContainsKey(rb))
+			if (!_originalValues.ContainsKey(rb))
 				return;
 
+			rb.drag = Mathf.Max(2f, depth / 6f);
+			rb.angularDrag = Mathf.Max(2f, depth / 6f);
+
+			// Don't apply any forces to the player.
 			if (mainscript.M.player.RB == rb)
-			{
-				rb.AddForce(Vector3.up * 100f * depth);
-				rb.drag = Mathf.Max(3f, depth);
-				rb.angularDrag = Mathf.Max(3f, depth);
 				return;
+
+			rb.AddForce(Vector3.up * 50f * depth / rb.mass);
+		}
+
+		private void UpdatePlayer(Rigidbody rb, float depth)
+		{
+			fpscontroller player = mainscript.M.player;
+			if (player.RB != rb)
+				return;
+
+			_playerDepth = depth;
+			
+			if (IsPlayerSwimming)
+			{
+				player.duckValue = 5f;
+				player.lastDucked = false;
+				player.Bduck = false;
+
+				// Reduce vertical velocity when not intentionally changing Y.
+				if (!player.input.duck && !player.input.jump)
+					rb.velocity = new Vector3(
+						rb.velocity.x,
+						Mathf.Lerp(rb.velocity.y, 0f, Time.fixedDeltaTime * 5f),
+						rb.velocity.z
+					);
 			}
 
-			rb.AddForce(Vector3.up * 100f * depth / rb.mass);
-			rb.drag = Mathf.Max(3f, depth);
-			rb.angularDrag = Mathf.Max(3f, depth);
+			if (player.Car == null)
+			{
+				float swimForce = Mathf.Max(10f, 10f * depth);
+				if (player.input.jump)
+				{
+					player.RB.AddForce(Vector3.up * swimForce);
+				}
+
+				if (player.input.duck)
+				{
+					player.RB.AddForce(Vector3.down * 60f);
+				}
+			}
+
+			if (depth > 1.5f)
+			{
+				_drown = mainscript.M.menu.TTankCap.transform.GetComponentInChildren<Slider>();
+				if (_drown == null)
+				{
+					_drown = Instantiate(mainscript.M.menu.SPiss, mainscript.M.menu.TTankCap.transform, false);
+					_drown.transform.Find("Fill Area").GetComponentInChildren<Image>().color = Color.cyan;
+					_drown.value = 0.0f;
+					_drown.maxValue = 1f;
+					_drown.fillRect.localScale = new Vector3(1f, 1f, 0f);
+					_drown.enabled = true;
+					_drown.gameObject.SetActive(true);
+					_drown.transform.Find("ImagePiss").gameObject.SetActive(false);
+				}
+				else
+				{
+					_drown.gameObject.SetActive(true);
+					_drown.value += 0.0015f * Time.deltaTime;
+					mainscript.M.player.deathSens = 0.5f;
+
+					Image slide = _drown.transform.Find("Fill Area").GetComponentInChildren<Image>();
+					slide.color = Color.cyan;
+					if (_drown.value > 0.75f)
+						slide.color = Color.red;
+
+					if (_drown.value >= 1f && !mainscript.M.died)
+						mainscript.M.player.Death();
+				}
+			}
 		}
 	}
 }
