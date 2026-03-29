c4e3ed52b2cc556d106b78f9dce2ac646ce615f5
unknown <miner717@hotmail.com>
2026-03-29 15:04:31 +0100

    feat: Alter world temperature for tropical and lava world types

---
+++ src/WorldTweaker/Harmony/TimePatches.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+
+namespace WorldTweaker.Harmony
+{
+	[HarmonyPatch(typeof(napszakvaltakozas), "FixedUpdate")]
+	internal static class Patch_napszakvaltakozas_FixedUpdate
+	{
+		private static void Postfix(napszakvaltakozas __instance)
+		{
+			// Alter world temperature based on world type.
+			switch (WorldTweaker.I.WorldType.Value)
+			{
+				// Tropical.
+				case 2f:
+					__instance.temp -= 5f;
+					break;
+				// Lava.
+				case 3f: 
+					__instance.temp += 30f;
+					break;
+			}
+		}
+	}
+}
